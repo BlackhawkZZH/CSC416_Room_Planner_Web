f704ac11c93824ba67d49f2568402c13e83f7253
BlackhawkZZH <zhangzhenhao79@gmail.com>
2022-12-05 15:54:34 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Welcome from "./pages/welcome/welcome"
+import './App.less';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
